ab58d7c2fac2914d8c2934dfca4dc6c2457b5d24
Nicola Calvio <info@nicolacalvio.com>
2022-08-18 11:11:50 +0200

    aggiunto mergesort

--- ordinamenti.h
+++ ordinamenti.h
@@ -8,7 +8,8 @@
 #endif //ASD_ORDINAMENTI_H
 
 void insertionSort(int *array, int n);
-void mergesort(int *array, int n);
+void merge(int *arr, int l, int m, int r);
+void mergeSort(int *array, int l, int r);
 void quicksort(int *array, int n);
 void heapsort(int *array, int n);
 void countingSort(int *array, int n);
@@ -27,12 +28,54 @@ void insertionSort(int *array, int n){
     }
 }
 
-void partition(int *array, int p, int q, int r){
+void merge(int *arr, int l, int m, int r){
+    int i, j, k;
+    int m_1 = m - l + 1;
+    int m_2 =  r - m;
 
+    int L[m_1], R[m_2];
+
+    for (i = 0; i < m_1; i++) L[i] = arr[l + i];
+    for (j = 0; j < m_2; j++) R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < m_1 && j < m_2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < m_1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < m_2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
+void mergeSort(int *array, int l, int r){
+    if (l < r)
+    {
+        int m = l+(r-l)/2;
 
-void mergeSort(int *array, int n){
+        mergeSort(array, l, m);
+        mergeSort(array, m+1, r);
 
+        merge(array, l, m, r);
+    }
 }
 
 
